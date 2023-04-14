9d21be9109e04d8334dba94ae9fe67591573c630
U-NICLEONIC\Johnson <Johnson@Nicleonic.Nex_Nickel>
2023-04-14 15:17:40 +0300

    Making the first function  'sun'  --Nicleonic--

--- prog.c
+++ prog.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+
+double sum(double arr[], int N){
+	double total = 0.0;
+	int counter = 0;
+	while(counter < N){
+		total+= arr[counter];
+		counter++;
+	}
+	return total;
+
+}
+
+
 int main(){
 	printf("Hello World!\nThis is the first modification\n");
 
