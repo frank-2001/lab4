4ee8f596c9aa4f15d7c6fe15cf1e4391f7f1ffb2
frank-2001 <frankmakolongo@gmail.com>
2023-04-14 14:54:35 +0200

    create function addTwoInt

--- prog.c
+++ prog.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+int addTwoInt(int nb1,int nb2){
+	return nb1+nb2;
+}
 int main(){
+	printf("Function AddTwoInt :%d \n",addTwoInt(10,10));
 	printf("Hello World!\nThis is the first modification\n");
-
 }
